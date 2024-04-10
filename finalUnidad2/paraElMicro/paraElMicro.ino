df490adf9b14d47845d4c9a025ec42cf0ca92cfb
Mafe-Garcia <mariaf.garcia@upb.edu.co>
2024-04-09 20:47:04 -0500

    codigo

--- finalUnidad2/paraElMicro/paraElMicro.ino
+++ finalUnidad2/paraElMicro/paraElMicro.ino
@@ -10,16 +10,9 @@ void task()
     static TaskStates taskState = TaskStates::INIT;
     uint32_t volumen[] ={0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
     static uint32_t toPrint = 0;
-    static uint32_t previousTime = 0;
-    static bool ledState = true;
+    static bool sigue = false;
   
-    uint32_t currentTime = millis();
-
-  if( (currentTime - previousTime) > 100){
-    previousTime = currentTime;
-    ledState = !ledState;
-    digitalWrite(LED_BUILTIN, ledState);
-  }
+    
 
     switch (taskState)
     {
@@ -36,6 +29,7 @@ void task()
             const char c = Serial.read();
             if (c == 's')
             {
+              Serial.println("dijo si");
               taskState = TaskStates::VARIABLE_CHANGE;
             }
           }          
@@ -43,17 +37,29 @@ void task()
         }
         case TaskStates::VARIABLE_CHANGE:
         {
+          Serial.println("escriba un numero entre 0 y 20");
+          do{
           if (Serial.available() > 0)
             {
+              if(Serial.read()> 20 | Serial.read()< 0)
+              {
+                Serial.println("debe estar entre 0 y 20");
+              }
+              else
+              {
               for(int i = 0; i < 21; i++)
               {
                 if (Serial.read() == i)
                 {
                   toPrint=volumen[i];
+                  Serial.println("si dio");
+                  sigue=true;
                   taskState = TaskStates::SEND_EVENT;
                 }                
               } 
-            }                  
+              }
+            }  
+          }while(sigue==false);                
           break;
         }
         case TaskStates::SEND_EVENT:
@@ -77,6 +83,16 @@ void setup()
 
 void loop()
 {
+  
+  static bool ledState = true;
+  uint32_t currentTime = millis();
+  static uint32_t previousTime = 0;
+
+  if( (currentTime - previousTime) > 100){
+    previousTime = currentTime;
+    ledState = !ledState;
+    digitalWrite(LED_BUILTIN, ledState);
+  }
   
     task();
 }
